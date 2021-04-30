e45d971b228bcdbff642ebb1d2ea7927fcb6fc55
Elias Ramirez <ramirezelias110@gmail.com>
2021-04-30 14:49:49 -0400

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,128 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="grid-container">
+    <header className="row">
+      <div>
+        <a className="brand" href="index.html">sneakerStore</a>
+      </div>
+      <div>
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign In</a>
+      </div>
+    </header>
+    
+    <main>
+      <div className="row center">
+        <div className="card">
+          <a href="product.html">
+            <img className="medium" src="./imgaes/dubzero.jpeg" alt="sneaker" />
+          </a>
+          <div className="card-body">
+            <a href="sneaker.html">
+              <h2>Air Jordan Dub Zero</h2>
+            <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+            </div>
+              <div className="price">
+                $320
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <a href="product.html">
+            <img className="medium" src="./imgaes/dubzero.jpeg" alt="sneaker"/>
+          </a>
+          <div className="card-body">
+            <a href="sneaker.html">
+              <h2>Air Jordan Dub Zero</h2>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                $320
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+            <img className="medium" src="./imgaes/dubzero.jpeg" alt="sneaker" />
+          </a>
+          <div className="card-body">
+            <a href="sneaker.html">
+              <h2>Air Jordan Dub Zero</h2>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                $320
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+            <img className="medium" src="./imgaes/dubzero.jpeg" alt="sneaker" />
+          </a>
+          <div className="card-body">
+            <a href="sneaker.html">
+              <h2>Air Jordan Dub Zero</h2>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                $320
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+            <img className="medium" src="./imgaes/dubzero.jpeg" alt="sneaker"/>
+          </a>
+          <div className="card-body">
+            <a href="sneaker.html">
+              <h2>Air Jordan Dub Zero</h2>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                $320
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer className="row center">
+      GA unit 3 project 
+    </footer>
+  </div>
+  );
+}
+
+export default App;
